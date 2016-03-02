491a32a7a509167f2047772bc0cf91ce5eed5502
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-03-02 15:53:12 +0800

    TODO: make the complete all button work

--- todo.rb
+++ todo.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
+require "pry"
 
 configure do
   enable :sessions
@@ -24,8 +26,18 @@ get "/lists/new" do
 end
 
 get "/lists/:index" do
-  @list = session[:lists][params[:index].to_i]
+  @index = params[:index].to_i
+  @list = session[:lists][@index]
+  @todos = session[:lists][@index][:todos]
+  @status = session[:lists][@index][:status]
   erb :list_view
+end
+
+get "/lists/:index/edit" do
+  @index = params[:index].to_i
+  @list = session[:lists][@index]
+
+  erb :edit_list
 end
 
 post "/lists" do
@@ -35,10 +47,71 @@ post "/lists" do
     session[:error] = error
     erb :new_list
   else
-    session[:lists] << { name: list_name, todos: [] }
+    session[:lists] << { name: list_name, todos: [], status: [] }
     session[:success] = 'Successfuly added the new list'
     redirect "/lists"
   end
+end
+
+post "/lists/:index" do
+  @index = params[:index].to_i
+  @list = session[:lists][@index]
+  @todos = session[:lists][@index][:todos]
+  list_name = params[:list_name].strip
+
+  if error = error_for_list_name?(list_name)
+    session[:error] = error
+    erb :edit_list
+  else
+    @list[:name] = params[:list_name]
+    session[:success] = 'Successfuly edited the list name'
+    redirect "/lists"
+  end
+end
+
+post "/lists/:index/destroy" do
+  @index = params[:index].to_i
+  session[:success] = "Successfuly deleted '#{session[:lists].delete_at(@index)[:name]}'"
+  redirect "/lists"
+end
+
+post "/lists/:index/todos" do
+  @index = params[:index].to_i
+  @list = session[:lists][@index]
+  @todos = session[:lists][@index][:todos]
+  @status = session[:lists][@index][:status]
+
+  todo_name = params[:todo].strip
+
+  if error = error_for_todo_name?(todo_name, @todos)
+    session[:error] = error
+    erb :list_view
+  else
+    @todos << todo_name
+    @status << ""
+    session[:success] = 'Successfuly added the todo.'
+    redirect "/lists/#{@index}"
+  end
+end
+
+post "/lists/:index/todos/:todo_idx/destroy" do
+  @index = params[:index].to_i
+  @list = session[:lists][@index]
+  @todos = session[:lists][@index][:todos]
+  @todo_idx = params[:todo_idx].to_i
+
+  session[:lists][@index][:status].delete_at(@todo_idx)
+  session[:success] = "Successfuly deleted '#{@todos.delete_at(@todo_idx)}' todo"
+  redirect "/lists/#{@index}"
+end
+
+post "/lists/:index/todos/:todo_idx/status" do
+  @index = params[:index].to_i
+  @todo_idx = params[:todo_idx].to_i
+  @status = session[:lists][@index][:status]
+
+  @status[@todo_idx] = params[:completed].empty? ? "complete" : ""
+  redirect "/lists/#{@index}"
 end
 
 helpers do
@@ -47,6 +120,14 @@ helpers do
       "List name must be between 1 and 100 characters"
     elsif session[:lists].any? { |list| list[:name] == list_name }
       "List name must be unique"
+    end
+  end
+
+  def error_for_todo_name?(todo_name, todos)
+    if !(1..100).cover? todo_name.size
+      "Todo name must be between 1 and 100 characters"
+    elsif todos.any? { |todo| todo == todo_name }
+      "Todo name must be unique"
     end
   end
 end
